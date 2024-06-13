89656ab5ff64fa4805337e853e478717b2aacc14
Net-assembly <s.cabotse@gmail.com>
2024-06-13 12:01:46 +0200

    Update solanaWebHook.php
    Correction

--- solanaWebHook.php
+++ solanaWebHook.php
@@ -1,5 +1,5 @@
 $inputData = file_get_contents("php://input");
-$jsonData = json_encode($inputData);
+$jsonData = json_decode($inputData);
 $secret = $jsonData->secret;
 if ($secret == "[YOURSECRET]") {
 	$paymentAmount = $jsonData->USD_Amount;
